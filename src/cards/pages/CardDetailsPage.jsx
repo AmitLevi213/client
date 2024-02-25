b847e9665be2641ced8fa7fa58240ed7d30336de
AmitLevi213 <amitandta@gmail.com>
2024-02-25 14:25:55 +0200

    First Commit

---
+++ src/cards/pages/CardDetailsPage.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import PageHeader from "../../components/PageHeader";
+import useCards from "../hooks/useCards";
+import CardHead from "../components/card/CardHead";
+
+const CardDetailsPage = () => {
+  const { id } = useParams();
+  const {
+    value: { card },
+    handleGetCardFromClient,
+  } = useCards();
+
+  useEffect(() => {
+    handleGetCardFromClient(id);
+  }, [handleGetCardFromClient, id]);
+
+  return (
+    <Container maxWidth="lg">
+      <PageHeader
+        title="Business Details"
+        subtitle="Here you can find all the information about the business you are looking for."
+      />
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item md={8} xs={12}>
+          {card && (
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Box mb={3}>
+                <CardHead image={card.image} />
+              </Box>
+              <Box>
+                <Typography variant="h4" gutterBottom>
+                  {card.title}
+                </Typography>
+                <Typography variant="h6" color="textSecondary" paragraph>
+                  {card.subtitle}
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  {card.description}
+                </Typography>
+                <Typography variant="subtitle1" paragraph>
+                  Phone: {card.phone}
+                </Typography>
+                <Typography variant="subtitle1" paragraph>
+                  Email: {card.email}
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default CardDetailsPage;
---
+++ src/users/pages/UserProfile.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useUser } from "../providers/UserProvider";
+import { Typography, Paper, Grid, Avatar } from "@mui/material";
+import { getUser } from "../services/usersApiService";
+import Spinner from "../../components/Spinner";
+import NavItem from "../../routes/NavItem";
+import ROUTES from "../../routes/routesModel";
+
+const UserProfile = () => {
+  const { user } = useUser();
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await getUser(user._id);
+        setCurrentUser(userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    if (user && user._id) {
+      fetchUser();
+    }
+  }, [user]);
+
+  if (!currentUser) {
+    return <Spinner justifyContent="center" alignItems="center" />;
+  }
+
+  return (
+    <Paper elevation={3} sx={{pt:2}}>
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={4}>
+          <Avatar
+            alt={currentUser.name}
+            src={currentUser.avatarUrl}
+            sx={{ width: 120, height: 120 }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Typography variant="h4" gutterBottom>
+            {currentUser.name.first} {currentUser.name.middle}{" "}
+            {currentUser.name.last}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            Email: {currentUser.email}
+          </Typography>
+          {/* Add more user information here */}
+        </Grid>
+        <Grid item xs={12}>
+          <NavItem
+            label="Edit Profile"
+            variant="contained"
+            color="inherit"
+            to={ROUTES.EDIT_USER}
+          ></NavItem>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default UserProfile;
