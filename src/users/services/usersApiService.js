5ddb89f7301ea94bc605b4518da2b065fabafa02
AmitLevi213 <amitandta@gmail.com>
2024-03-18 23:51:38 +0200

    fixed everything

--- src/users/services/usersApiService.js
+++ src/users/services/usersApiService.js
@@ -3,11 +3,7 @@ import axios from "axios";
 const apiUrl =
   process.env.REACT_APP_API_URL ||
   "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users";
-
-const cardsApi =
-  process.env.REACT_APP_API_URL ||
-  "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards";
-
+  
 export const login = async (user) => {
   try {
     const { data } = await axios.post(`${apiUrl}/login`, user);
@@ -36,14 +32,6 @@ export const editUser = async (user, id) => {
 export const getUser = async (id) => {
   try {
     const { data } = await axios.get(`${apiUrl}/${id}`);
-    return data;
-  } catch (error) {
-    return Promise.reject(error.message);
-  }
-};
-export const getCardsByUser = async (id, card) => {
-  try {
-    const { data } = await axios.get(`${cardsApi}/${id}`, card);
     return data;
   } catch (error) {
     return Promise.reject(error.message);
